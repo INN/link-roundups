aa450f873ab58cc711dc7b5e99047ccb774d0cfd
Ryan Nagle <rmnagle@gmail.com>
2015-05-26 14:38:03 -0500

    stubbing out some test functions, completing some tests for argo-links.php file

--- tests/test-argo-link-roundups.php
+++ tests/test-argo-link-roundups.php
@@ -1,14 +1,43 @@
 <?php
 
 class ArgoLinkRoundupsTestFunctions extends WP_UnitTestCase {
-
-	function setUp() {
-		parent::setUp();
-
-	}
-	
-	function testTest() {
-		$this->assertTrue(true, "True was not true?");
+	function test_init() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
 	}
 
+	function test_my_get_posts() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_register_post_type() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_add_custom_post_fields() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_display_custom_fields() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_save_custom_fields() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_add_argo_link_roundup_options_page() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_register_mysettings() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_validate_mailchimp_integration() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_build_argo_link_roundups_options_page() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
 }
--- tests/test-argo-links.php
+++ tests/test-argo-links.php
@@ -1,8 +1,32 @@
 <?php
+
 class ArgoLinksTestFunctions extends WP_UnitTestCase {
 
 	function setUp() {
 		parent::setUp();
+
+		// Set up global $post object
+		$this->argolinks_post = $this->factory->post->create(array('post_type' => 'argolinkroundups'));
+		global $post;
+		$this->tmp_post = $post;
+		$post = get_post($this->argolinks_post);
+		setup_postdata($post);
+
+		update_option('argo_link_roundups_use_mailchimp_integration', 'on');
+
+		$this->mc_api_endpoint = 'us10';
+		update_option('argo_link_roundups_mailchimp_api_key', 'TKTK-' . $this->mc_api_endpoint);
+
+		// Mimic the post edit page
+		set_current_screen('post');
+		$screen = get_current_screen();
+		$screen->post_type = 'argolinkroundups';
+	}
+
+	function tearDown() {
+		// Reset global $post object
+		$post = $this->tmp_post;
+		wp_reset_postdata();
 	}
 
 	function test_argolinks_activation() {
@@ -30,6 +54,40 @@ class ArgoLinksTestFunctions extends WP_UnitTestCase {
 		// Testing when it should not run
 		$ret = argo_flush_permalinks();
 		$this->assertFalse($ret);
+	}
 
+	function test_argo_links_create_mailchimp_campaign_button() {
+		// Test the function output
+		$this->expectOutputRegex('/Create MailChimp Campaign/');
+		argo_links_create_mailchimp_campaign_button();
+	}
+
+	function test_argo_links_enqueue_assets() {
+		argo_links_enqueue_assets();
+
+		global $wp_styles, $wp_scripts;
+		$this->assertTrue(!empty($wp_scripts->registered['argo-link-roundups']));
+		$this->assertTrue(!empty($wp_styles->registered['argo-links-common']));
+	}
+
+	function test_argo_links_modal_underscore_template() {
+		$this->expectOutputRegex('/id="argo-links-modal-tmpl"/');
+		argo_links_modal_underscore_template();
+	}
+
+	function test_argo_links_json_obj() {
+		$serializable_obj = argo_links_json_obj();
+		$this->assertEquals($this->argolinks_post, $serializable_obj['post_id']);
+		$this->assertEquals($this->mc_api_endpoint, $serializable_obj['mc_api_endpoint']);
+	}
+
+	function test_argo_links_add_modal_template() {
+		$this->expectOutputRegex('/var AL =/');
+		argo_links_add_modal_template();
+	}
+
+	function test_argo_links_get_mc_api_endpoint() {
+		$ret = argo_links_get_mc_api_endpoint();
+		$this->assertEquals($ret, $this->mc_api_endpoint);
 	}
 }
