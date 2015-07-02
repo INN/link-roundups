fa8cb8189a592a265b3a0699452d21991c218477
David Ryan <david@inn.org>
2015-07-01 19:16:34 -0700

    link roundup options page cleanup

--- templates/options.php
+++ templates/options.php
@@ -4,17 +4,26 @@ $default_html = <<<EOT
 EOT;
 ?>
 <div class="wrap">
-	<h2>Argo Link Roundups</h2>
+	<h2>Link Roundups Options</h2>
 
 	<?php settings_errors(); ?>
+	
+	<h4>Documentation</h4>
+	Read about these settings and using this plugin in <a href="https://github.com/INN/link-roundups/tree/master/docs">the documentation on GitHub</a>.
 
 	<form method="post" action="options.php">
 		<?php settings_fields( 'argolinkroundups-settings-group' ); ?>
 		<?php do_settings_fields( 'argolinkroundups-settings-group', null ); ?>
 		<table class="form-table">
 			<tr valign="top">
-				<th scope="row">Custom Url Slug</th>
+				<th scope="row">Custom URL Slug</th>
 				<td><input type="text" name="argo_link_roundups_custom_url" value="<?php echo get_option('argo_link_roundups_custom_url'); ?>" /></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><p>Overwrites <code>roundup</code> as the slug in Link Roundup URLs.</p><br />
+			    <strong>IMPORTANT</strong>: After saving a custom slug, you <strong>must</strong> update your Permalink Settings (Settings --> Permalinks).
+				</td>
 			</tr>
 			<tr valign="top">
 				<th scope="row">Custom HTML</th>
@@ -23,18 +32,26 @@ EOT;
 			<tr>
 				<td></td>
 				<td>
-				<em>(You will need to use single quotes in your html above; all double quotes will be automatically converted to single quotes before use)</em><br />
-				You can use the above field to customize the html that is output for each link.	The following tags will be replaced with the url, title, description, and source automatically when the link is pushed into the editor.<br />
-				#!URL!#, #!TITLE!#, #!DESCRIPTION!#, #!SOURCE!#<br />
+				<h4 style="margin-top:0;padding-top:0;">Modify the output of Saved Links sent to the editor.</h4>
+				<em>Single quotes are REQUIRED in Custom HTML. Double quotes will be automatically converted to single quotes before use.</em><br /><br />
+				The following tags will be replaced with the URL, Title, Description, and Source automatically when the Saved Link is pushed into the Post Editor.<br />
+				<ul style="list-style-type:square;">
+				<li><code>#!URL!#</code></li>
+				<li><code>#!TITLE!#</code></li>
+				<li><code>#!DESCRIPTION!#</code></li>
+				<li><code>#!SOURCE!#</code></li>
+				</ul>
+				<br /><br />
 				The current default html for reference is:<br />
-				<?php echo htmlspecialchars($default_html); ?><br />
-				<em>(Please note that you will have to update your style.css file for your theme to style your new html)</em><br />
+				<code><?php echo htmlspecialchars($default_html); ?></code><br />
+				<h5>Style Links</h5>
+				Add custom styles to your theme's CSS based on selectors and structure above.
 				</td>
 			</tr>
 			<tr>
 				<th scope="row">MailChimp Integration</th>
 				<td>
-					<p>
+					<p style="margin-bottom:5px;">
 						<label for="argo_link_roundups_use_mailchimp_integration">
 							Enable MailChimp Integration?
 							<input type="checkbox" name="argo_link_roundups_use_mailchimp_integration"
@@ -49,6 +66,7 @@ EOT;
 								placeholder="Mailchimp API Key" />
 						</label>
 					</p>
+					<p><a href="http://kb.mailchimp.com/accounts/management/about-api-keys#Find-or-Generate-Your-API-Key">Finding your key</a></p>
 				</td>
 			</tr>
 			<?php if ((bool) get_option('argo_link_roundups_use_mailchimp_integration') && !empty($templates)) { ?>
