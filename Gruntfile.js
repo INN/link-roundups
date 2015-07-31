3651aa93f114ad78994c814bdd98036078c430be
aschweigert <aschweigert@gmail.com>
2015-07-31 14:42:54 -0400

    add translation stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -70,6 +70,38 @@ module.exports = function(grunt) {
                     'cssmin'
                 ]
             },
+        },
+        pot: {
+            options: {
+                text_domain: 'link-roundups',
+                dest: 'lang/',
+                keywords: [ //WordPress localization functions
+                    '__:1',
+                    '_e:1',
+                    '_x:1,2c',
+                    'esc_html__:1',
+                    'esc_html_e:1',
+                    'esc_html_x:1,2c',
+                    'esc_attr__:1',
+                    'esc_attr_e:1',
+                    'esc_attr_x:1,2c',
+                    '_ex:1,2c',
+                    '_n:1,2',
+                    '_nx:1,2,4c',
+                    '_n_noop:1,2',
+                    '_nx_noop:1,2,3c'
+                ]
+            },
+            files: {
+                src: '**/*.php',
+                expand: true
+            }
+        },
+        po2mo: {
+            files: {
+                src: 'lang/*.po',
+                expand: true
+            }
         }
     });
 
