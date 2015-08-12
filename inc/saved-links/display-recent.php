cd5bb8caf3636ed35db5fc2820a8894f09ecc914
Ben Keith <ben@benlk.com>
2015-08-12 12:20:42 -0400

    Okay, the links aren't displaying, but that's because the query isn't coming back filled. But let's create some buttons.

--- inc/saved-links/display-recent.php
+++ inc/saved-links/display-recent.php
@@ -40,14 +40,37 @@ class Saved_Links_List_Table extends clone_WP_List_Table {
 	}
 
 	function extra_tablenav( $which ) {
+		// this will display at top and bottom
+		?>
+		<button class='button append-saved-links'><?php _e( 'Send to Editor', 'link-roundups' ); ?></button>
+		<?php
 		if ( $which == 'top' ) {
-			echo 'foo';
 			// Date range:
+			?>
+			<div >
+			<form action='' method='get' id='filter_links'>
+				<label for='link_date'><b><?php _e( 'Date Range:', 'link-roundups' ); ?></b></label>
+				<select name='link_date'>
+					<option value='today' <?php echo ( ( isset( $_REQUEST['link_date'] ) && $_REQUEST['link_date'] == 'today' ) ? 'selected' : '' );?>><?php _e( 'Today',' link-roundups' ); ?></option>
+					<option value='this_week' <?php echo ( ( isset( $_REQUEST['link_date'] ) && $_REQUEST['link_date'] == 'this_week' ) ? 'selected' : '' );?>><?php _e( 'This Week',' link-roundups' ); ?></option>
+					<option value='this_month' <?php echo ( ( isset( $_REQUEST['link_date']) && $_REQUEST['link_date'] == 'this_month' ) ? 'selected' : '' );?>><?php _e( 'This Month',' link-roundups' ); ?></option>
+					<option value='this_year' <?php echo ( ( isset( $_REQUEST['link_date'] ) && $_REQUEST['link_date'] == 'this_year' ) ? 'selected' : '' );?>><?php _e( 'This Year',' link-roundups' ); ?></option>
+					<option value='show_all' <?php echo ( ( isset( $_REQUEST['link_date'] ) && $_REQUEST['link_date'] == 'show_all' ) ? 'selected' : '' );?>><?php _e( 'Show All',' link-roundups' ); ?></option>
+				</select>
+				<?php if( isset( $_REQUEST['orderby'] ) ) : ?>
+					<input type='hidden' name='orderby' value='<?php echo $_REQUEST['orderby']; ?>'/>
+				<?php endif;?>
+				<?php if( isset($_REQUEST['order'] ) ) : ?>
+					<input type='hidden' name='order' value='<?php echo $_REQUEST['order']; ?>'/>
+				<?php endif;?>
+				<input class='button' type='submit' value='Filter'/>
+			</form>
+		</div>
+		<?php
 		}
 		if ( $which == 'bottom' ) {
-			echo 'bar';
+			// Nothing to see here.
 		}
-		// send to editor (both)
 	}
 
 	function get_columns() {
@@ -131,6 +154,7 @@ class Saved_Links_List_Table extends clone_WP_List_Table {
 		// Fetch the items
 		$links_query = new WP_Query($args);
 		$this->items = $links_query->posts;
+		var_log($links_query->posts);
 		// So where smash magazine uses wpdb->get_results, what WP_Query does is:
 		//	wp_query->get_posts()
 		//		wpdb->get_results
@@ -138,216 +162,56 @@ class Saved_Links_List_Table extends clone_WP_List_Table {
 		// However, we can do whatever we want with these results, because $this->items is then parsed in $this->display_rows, which runs $this->single_row($item) for each item, which wraps $this->single_row_columns($item) in a <tr>, which does most of the dirty work I think.
 	}
 
+	/*
+	 * Turn a WP_Post object into a column
+	 *
+	 * @param $item WP_Post object
+	 */
+	function single_row_columns() {
+		list( $columns, $hidden, $sortable, $primary ) = $this->get_column_info();
+		foreach ( $columns as $column_name => $column_display_name ) {
+			$classes = "$column_name column-$column_name";
+			if ( $primary === $column_name ) {
+				$classes .= ' has-row-actions column-primary';
+			}
+			if ( in_array( $column_name, $hidden ) ) {
+				$classes .= ' hidden';
+			}
+			// Comments column uses HTML in the display name with screen reader text.
+			// Instead of using esc_attr(), we strip tags to get closer to a user-friendly string.
+			$data = 'data-colname="' . wp_strip_all_tags( $column_display_name ) . '"';
+			$attributes = "class='$classes' $data";
+			if ( 'cb' == $column_name ) {
+				echo '<th scope="row" class="check-column">';
+				echo $this->column_cb( $item );
+				echo '</th>';
+			} elseif ( method_exists( $this, '_column_' . $column_name ) ) {
+				echo call_user_func(
+					array( $this, '_column_' . $column_name ),
+					$item,
+					$classes,
+					$data,
+					$primary
+				);
+			} elseif ( method_exists( $this, 'column_' . $column_name ) ) {
+				echo "<td $attributes>";
+				echo call_user_func( array( $this, 'column_' . $column_name ), $item );
+				echo $this->handle_row_actions( $item, $column_name, $primary );
+				echo "</td>";
+			} else {
+				echo "<td $attributes>";
+				echo $this->column_default( $item, $column_name );
+				echo $this->handle_row_actions( $item, $column_name, $primary );
+				echo "</td>";
+			}
+		}
+	}
 }
 
-// count the total number of posts
-$the_query = new WP_Query( array_merge( $args, array( 'posts_per_page' => $posts_per_page, 'paged' => $page ) ) );
-$from_result = 1;
-$to_result = $posts_per_page;
-if ($page != 1) {
-  $from_result = $posts_per_page * ( $page - 1 );
-  $to_result = $from_result + $posts_per_page - 1;
-}
-if ($to_result > $total_post_count)
-  $to_result = $total_post_count;
+$links_list_table = new Saved_Links_List_Table();
+$links_list_table->prepare_items();
+$links_list_table->display();
 
-// $query_url is used in building the pagination buttons
-$query_url = '';
-?>
-<div class='display-saved-links'>
-  <div class='pagination'>
-    <div style='float:left'>
-      <button class='button append-saved-links'><?php _e( 'Send to Editor', 'link-roundups' ); ?></button>
-      <form action='' method='get' id='filter_links'>
-        <label for='link_date'><b><?php _e( 'Date Range:', 'link-roundups' ); ?></b></label>
-        <select name='link_date'>
-          <option value='today' <?php echo ( ( isset( $_REQUEST['link_date'] ) && $_REQUEST['link_date'] == 'today' ) ? 'selected' : '' );?>><?php _e( 'Today',' link-roundups' ); ?></option>
-          <option value='this_week' <?php echo ( ( isset( $_REQUEST['link_date'] ) && $_REQUEST['link_date'] == 'this_week' ) ? 'selected' : '' );?>><?php _e( 'This Week',' link-roundups' ); ?></option>
-          <option value='this_month' <?php echo ( ( isset( $_REQUEST['link_date']) && $_REQUEST['link_date'] == 'this_month' ) ? 'selected' : '' );?>><?php _e( 'This Month',' link-roundups' ); ?></option>
-          <option value='this_year' <?php echo ( ( isset( $_REQUEST['link_date'] ) && $_REQUEST['link_date'] == 'this_year' ) ? 'selected' : '' );?>><?php _e( 'This Year',' link-roundups' ); ?></option>
-          <option value='show_all' <?php echo ( ( isset( $_REQUEST['link_date'] ) && $_REQUEST['link_date'] == 'show_all' ) ? 'selected' : '' );?>><?php _e( 'Show All',' link-roundups' ); ?></option>
-        </select>
-        <?php if( isset( $_REQUEST['orderby'] ) ) : ?>
-          <input type='hidden' name='orderby' value='<?php echo $_REQUEST['orderby']; ?>'/>
-        <?php endif;?>
-        <?php if( isset($_REQUEST['order'] ) ) : ?>
-          <input type='hidden' name='order' value='<?php echo $_REQUEST['order']; ?>'/>
-        <?php endif;?>
-        <input class='button' type='submit' value='Filter'/>
-      </form>
-    </div>
-    <div class="page-navi" style='float:right'>
-      <?php echo $from_result . ' - ' . $to_result . ' ' . __( 'of', 'link-roundups' ) . ' ' . $total_post_count; ?>
-      <?php if(!($page <= 6)):?>
-        <a class="button" href='lroundups_page=<?php echo $pagination_first;?><?php echo $query_url; ?>' title='First'>&laquo;</a>
-      <?php endif; ?>
-      <?php if(!($page == 1)):?>
-        <a class="button" href='lroundups_page=<?php echo $pagination_previous;?><?php echo $query_url; ?>' title='Previous'>&laquo;</a>
-      <?php endif; ?>
-      <?php
-        $start = 1;
-        $count = 0;
-        if ( $page == 1 ) {
-          if ( $pagination_last >= 11 ) {
-            $count = $start + 10;
-          } else {
-            $count = $pagination_last;
-          }
-        } else if ( $page == $pagination_last ) {
-          if ( $pagination_last <= 11 ) {
-            $start = 1;
-            $count = $pagination_last;
-          } else {
-            $start = $pagination_last - 11;
-            $count = $start + 11;
-          }
-        } else {
-          if( $pagination_last <= 11 ) {
-            $start = 1;
-            $count = $pagination_last;
-          } else if ( ( $page + 5 ) > $pagination_last ) {
-            $start = $pagination_last - 10;
-            $count = $start + 10;
-          } else if ( ( $page - 5 ) <= 0 ) {
-            $start = 1;
-            $count = 11;
-          } else {
-            $start = $page - 5;
-            $count = $start + 10;
-          }
-        }
-        while ( $start <= $count ) {
-          echo "<a href='lroundups_page=$start$query_url' class='button " . ( $start == $page ? 'current' : '' ) . "'>$start</a> &nbsp;";
-          $start++;
-        }
-      ?>
-      <?php if ( !( $page == $pagination_last ) ) : ?>
-        <a class="button" href='lroundups_page=<?php echo $pagination_next;?><?php echo $query_url; ?>' title='Next'>&raquo;</a>
-      <?php endif; ?>
-      <?php if ( !( $page >= ( $pagination_last - 5 ) ) ) : ?>
-        <a class="button" href='lroundups_page=<?php echo $pagination_last;?><?php echo $query_url; ?>' title='Last'>&raquo;</a>
-      <?php endif;?>
-    </div>
-  </div>
-
-  <!-- START TABLE -->
-  <table class="wp-list-table widefat fixed posts" cellspacing="0">
-    <tr>
-      <th scope="col" id="cb" class="manage-column column-cb check-column" style=""><input type="checkbox" id='check-all-boxes'></th>
-      <th scope="col" id="title" class="manage-column column-title <?php echo (isset($_REQUEST['orderby']) && $_REQUEST['orderby'] == 'title' ? 'sorted' : 'sortable');?> <?php echo (isset($_REQUEST['orderby']) ? ($_REQUEST['orderby'] == 'title' && $_REQUEST['order'] == 'desc' ? 'desc' : 'asc') : 'desc');?>" style=""><a href="post_type=rounduplink&orderby=title&order=<?php echo (isset($_REQUEST['orderby']) ? ($_REQUEST['orderby'] == 'title' && $_REQUEST['order'] == 'desc' ? 'asc' : 'desc') : 'desc');?><?php echo (isset($_REQUEST['link_date']) ? '&link_date='.$_REQUEST['link_date']: ''); ?>"><span>Title</span><span class="sorting-indicator"></span></a></th>
-      <th scope="col" id="author" class="manage-column column-author <?php echo (isset($_REQUEST['orderby']) && $_REQUEST['orderby'] == 'author' ? 'sorted' : 'sortable');?> <?php echo (isset($_REQUEST['orderby']) ? ($_REQUEST['orderby'] == 'author' && $_REQUEST['order'] == 'desc' ? 'desc' : 'asc') : 'desc');?>" style=""><a href="post_type=rounduplink&orderby=author&order=<?php echo (isset($_REQUEST['orderby']) ? ($_REQUEST['orderby'] == 'author' && $_REQUEST['order'] == 'desc' ? 'asc' : 'desc') : 'desc');?><?php echo (isset($_REQUEST['link_date']) ? '&link_date='.$_REQUEST['link_date']: ''); ?>"><span>Author</span><span class="sorting-indicator"></span></a></th>
-      <th scope="col" id="link-tags" class="manage-column column-link-tags" style="">Tags</th>
-      <th scope="col" id="date" class="manage-column column-date <?php echo (isset($_REQUEST['orderby']) && $_REQUEST['orderby'] == 'date' ? 'sorted' : 'sortable');?> <?php echo (isset($_REQUEST['orderby']) ? ($_REQUEST['orderby'] == 'date' && $_REQUEST['order'] == 'desc' ? 'desc' : 'asc') : 'desc');?>" style=""><a href="post_type=rounduplink&orderby=date&order=<?php echo (isset($_REQUEST['orderby']) ? ($_REQUEST['orderby'] == 'date' && $_REQUEST['order'] == 'desc' ? 'asc' : 'desc') : 'desc');?><?php echo (isset($_REQUEST['link_date']) ? '&link_date='.$_REQUEST['link_date']: ''); ?>"><span>Date</span><span class="sorting-indicator"></span></a></th>
-    </tr>
-
-    <?php $i=1; ?>
-    <?php if ($the_query->have_posts()) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-      <tr id='<?php echo get_the_ID(); ?>' class='<?php echo ($i%2 ? 'alternate' : '')?>'>
-        <th scope="row" id="cb" class="manage-column column-cb check-column" style="">
-          <input type="checkbox" class='lroundups-link' value='<?php echo get_the_ID(); ?>'/>
-        </th>
-        <td scope="row" id="title" class="manage-column column-title sortable desc" style="">
-          <span id="title-<?php echo get_the_ID();?>"><?php echo the_title(); ?></span><br />
-          <?php
-          $custom = get_post_custom($post->ID);
-          ?>
-          <span id='url-<?php echo get_the_ID();?>'style='font-size:10px;'><em><?php echo (isset($custom["argo_link_url"][0]) ? $custom["argo_link_url"][0] : ''); ?></em></span>
-          <span id='description-<?php echo get_the_ID();?>'style='display:none;'><em><?php echo (isset($custom["argo_link_description"][0]) ? $custom["argo_link_description"][0] : ''); ?></em></span>
-          <span id='source-<?php echo get_the_ID();?>'style='display:none;'><em><?php echo (isset($custom["argo_link_source"][0]) ? $custom["argo_link_source"][0] : ''); ?></em></span>
-
-        </td>
-        <td scope="row" id="author" class="manage-column column-author sortable desc" style=""><span><?php the_author();?></span></td>
-        <td scope="row" id="link-tags" class="manage-column column-link-tags" style="">
-        <?php
-        $terms = get_the_terms(get_the_ID(), 'argo-link-tags');
-            if (count($terms) > 1) {
-              foreach ($terms as $term) {
-                echo $term->name.", ";
-              }
-            } else {
-              echo "&nbsp;";
-            }
-            $terms = "";
-        ?>
-        </td>
-        <td scope="row" id="date" class="manage-column column-date sortable asc" style=""><span><?php echo get_the_date(); ?></span></td>
-      </tr>
-      <?php $i++;?>
-    <?php endwhile; else : // below is the blank template if no links are found ?>
-      <tr id="blank" class='alternate'>
-        <th scope="row" id="cb" class="manage-column column-cb check-column" style=""></th>
-        <td scope="row" id="title" class="manage-column column-title sortable desc" style="">
-          <span class="none-found"><?php _e('No links found.', 'link-roundups'); ?></span><small><?php _e('Try selecting a different Date Range above.', 'link-roundups'); ?></small><br />
-        <td scope="row" id="author" class="manage-column column-author sortable desc" style=""><span>&nbsp;</span></td>
-        <td scope="row" id="link-tags" class="manage-column column-link-tags" style="">
-        	 
-        </td>
-        <td scope="row" id="date" class="manage-column column-date sortable asc" style=""><span>&nbsp;</span></td>
-      </tr>
-      <?php $hide = true; ?>
-    <?php endif; ?>
-  </table>
-
-  <div class='pagination'>
-    <div style='float:left'>
-      <button class='button append-saved-links'>Send to Editor</button>
-    </div>
-    <div class="page-navi" style='float:right'>
-      <br />
-      <?php echo $from_result . ' - ' . $to_result . ' ' . __( 'of', 'link-roundups' ) . ' ' . $total_post_count; ?>
-      <?php if(!($page <= 6)):?>
-        <a class="button" href='lroundups_page=<?php echo $pagination_first;?><?php echo $query_url; ?>' title='First'>&laquo;</a>
-      <?php endif; ?>
-      <?php if(!($page == 1)):?>
-        <a class="button" href='lroundups_page=<?php echo $pagination_previous;?><?php echo $query_url; ?>' title='Previous'>&laquo;</a>
-      <?php endif; ?>
-      <?php
-        $start = 1;
-        $count = 0;
-        if ($page == 1) {
-          if ($pagination_last >= 11) {
-            $count = $start + 10;
-          } else {
-            $count = $pagination_last;
-          }
-        } else if ($page == $pagination_last) {
-          if ($pagination_last <= 11) {
-            $start = 1;
-            $count = $pagination_last;
-          } else {
-            $start = $pagination_last - 11;
-            $count = $start + 11;
-          }
-        } else {
-          if($pagination_last <= 11) {
-            $start = 1;
-            $count = $pagination_last;
-          } else if (($page + 5) > $pagination_last) {
-            $start = $pagination_last - 10;
-            $count = $start + 10;
-          } else if (($page - 5) <= 0) {
-            $start = 1;
-            $count = 11;
-          } else {
-            $start = $page - 5;
-            $count = $start + 10;
-          }
-        }
-        while ($start <= $count) {
-          echo "<a href='lroundups_page=$start$query_url' class='button ".($start == $page ? 'current' : '')."'>$start</a> &nbsp;";
-          $start++;
-        }
-      ?>
-      <?php if(!($page == $pagination_last)):?>
-        <a class="button" href='lroundups_page=<?php echo $pagination_next;?><?php echo $query_url; ?>' title='Next'>&raquo;</a>
-      <?php endif; ?>
-      <?php if(!($page >= ($pagination_last - 5))):?>
-        <a class="button" href='lroundups_page=<?php echo $pagination_last;?><?php echo $query_url; ?>' title='Last'>&raquo;</a>
-      <?php endif;?>
-    </div>
-  </div>
-</div>
-<?php
 // Reset Query
 wp_reset_query();
 
