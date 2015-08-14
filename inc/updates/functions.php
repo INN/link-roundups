0b2180955200abc86946014d0eac06f81189c31f
Ryan Nagle <rmnagle@gmail.com>
2015-08-14 16:09:24 -0500

    merge most recent stuff from master, fix a few update functions to run for v0.3.2 update, code formatting

--- inc/updates/functions.php
+++ inc/updates/functions.php
@@ -15,6 +15,35 @@ function lroundups_update_post_types( $to, $from ) {
 add_action( 'lroundups_update_0.3', 'lroundups_update_post_types', 10, 2 );
 
 /**
+ * Migrates old options from argo-links
+ *
+ * @since 0.3
+ */
+function lroundups_migrate_options() {
+	$old_options = array(
+		'argo_link_roundups_custom_url',
+		'argo_link_roundups_custom_html',
+		'link_roundups_custom_name_singular',
+		'link_roundups_custom_name_plural',
+		'argo_link_roundups_use_mailchimp_integration',
+		'argo_link_roundups_mailchimp_api_key',
+		'argo_link_mailchimp_template',
+		'argo_link_mailchimp_list',
+	);
+
+	// check for old options and replace the old prefixes with lroundups_
+	foreach ( $old_options as $old_option ) {
+		$new_option = str_replace(
+			array('argo_link_roundups_', 'link_roundups_'), 'lroundups_', $old_option);
+		$old_value = get_option($old_option);
+		$result = update_option($new_option, $old_value);
+		if ($result)
+			delete_option($old_option);
+	}
+}
+add_action( 'lroundups_update_0.3', 'lroundups_migrate_options', 10 );
+
+/**
  * Update rounduplink post meta in database.
  *
  *  · argo_link_url → lr_url
@@ -44,12 +73,26 @@ add_action( 'lroundups_update_0.3.2', 'lroundups_update_post_terms', 15, 2 );
  * Update taxonomy name
  *  · argo-link-tags → lr-tags
  *
- * @since 0.3.1
+ * @since 0.3.2
  */
 function lroundups_update_taxonomy_name( $to, $from ) {
 	_lroundups_convert_taxonomy_name( 'argo-link-tags', 'lr-tags' );
 }
-add_action( 'lroundups_update_0.3.1', 'lroundups_update_taxonomy_name', 20, 2 );
+add_action( 'lroundups_update_0.3.2', 'lroundups_update_taxonomy_name', 20, 2 );
+
+/**
+ * Get rid of an old option
+ *
+ * @since 0.3.2
+ */
+function lroundups_delete_option() {
+	delete_option('argolinks_flush');
+}
+add_action( 'lroundups_update_0.3.2', 'lroundups_delete_argolinks_flush_option', 10 );
+
+/*
+ * Update utility functions below
+ */
 
 /**
  * Migrate any custom post type to another
@@ -84,7 +127,7 @@ function _lroundups_convert_posts_meta( $old_meta, $new_meta ) {
 /**
  * Migrate a custom taxonomy to a new key
  *
- * @since 0.3.1
+ * @since 0.3.2
  */
 function _lroundups_convert_taxonomy_name( $old_tax, $new_tax ) {
 	global $wpdb;
@@ -95,42 +138,3 @@ function _lroundups_convert_taxonomy_name( $old_tax, $new_tax ) {
 		, $new_tax, $old_tax )
 	);
 }
-
-/**
- * Migrates old options from argo-links
- *
- * @since 0.3
- */
-function lroundups_migrate_options() {
-	$old_options = array(
-		'argo_link_roundups_custom_url',
-		'argo_link_roundups_custom_html',
-		'link_roundups_custom_name_singular',
-		'link_roundups_custom_name_plural',
-		'argo_link_roundups_use_mailchimp_integration',
-		'argo_link_roundups_mailchimp_api_key',
-		'argo_link_mailchimp_template',
-		'argo_link_mailchimp_list',
-	);
-
-	// check for old options and replace the old prefixes with lroundups_
-	foreach ( $old_options as $old_option ) {
-		$new_option = str_replace(
-			array('argo_link_roundups_', 'link_roundups_'), 'lroundups_', $old_option);
-		$old_value = get_option($old_option);
-		$result = update_option($new_option, $old_value);
-		if ($result)
-			delete_option($old_option);
-	}
-}
-add_action( 'lroundups_update_0.3', 'lroundups_migrate_options', 10 );
-
-/**
- * Get rid of an old option
- *
- * @since 0.3.2
- */
-function lroundups_delete_option() {
-	delete_option('argolinks_flush');
-}
-add_action( 'lroundups_update_0.3.2', 'lroundups_delete_argolinks_flush_option', 10 );
